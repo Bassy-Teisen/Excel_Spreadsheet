3f355dec88ddef10891d4c57069ccd9a26d6dc1d
Bassy_Teisen <cab022111@coderacademy.edu.au>
2022-05-14 13:14:50 +1000

    generating grid using stored key values for cells

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
 const grid = document.getElementById("grid");
+const inputArray = []
 
-// create input elements for grid
-function makeGrid(rows, cols) {
+makeGrid(10, 10)
+
+
+// create object containing the key value of the input cells
+function makeInputId(rows, cols) {
+
     for (r = 0; r < (rows + 1 ); r++) {
         for (c = 0; c < (cols ); c++) {
-            const inputCell = document.createElement("input");
-            let cell = convertToNumberingScheme(c ).concat(r)
-            inputCell.id = cell;
-            inputCell.placeholder = cell;
-            inputCell.type = "text"
-            grid.appendChild(inputCell);
-
+            const inputObject = {}
+           
+            let cellId = convertToNumberingScheme(c ).concat(r)
+            inputObject[cellId] = "null"
+            inputArray.push(inputObject)
         }
     }
 }
 
-makeGrid(10,10)
+
+
+// create input elements for grid using the array of key value objects
+function makeGrid(rows, cols) {
+    makeInputId(rows, cols)
+    for (i = 0; i < (rows * cols ); i++) {
+        const cellKey = Object.keys(inputArray[i])
+        const cellValue = Object.values(inputArray[i])
+
+        const inputCell = document.createElement("input");
+
+        if (cellValue != "null") {
+            inputCell.value = cellValue
+        } 
+
+        inputCell.id = cellKey;
+        inputCell.placeholder = cellKey;
+        inputCell.type = "text"
+        grid.appendChild(inputCell);
+        
+    }
+}
 
 
 
